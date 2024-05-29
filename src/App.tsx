22f02491aa7832439e22ff831844b267e32c1713
Naico Wang <wanghy1984@gmail.com>
2024-05-29 12:01:45 +0800

    fix(gh-pages): fix cname file missing issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from './pages/Home/index'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          Test for GH Pages
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/about" element='about' />
+        <Route path="/dashboard" element='sdaf' />
+        <Route path="*" element='' />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.less'
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <aside className='aside'>this is homepage</aside>
+    </React.Fragment>
+  )
+}
+
+export default Home
