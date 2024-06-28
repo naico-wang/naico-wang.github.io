0c510461d3557c38e0c31e54f9d8a3a73e0b4d16
Naico Wang <>
2024-06-28 23:06:30 +0800

    feat(comment): add giscus as comment system

---
+++ src/.vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Giscus from '@giscus/vue';
+import { useRoute, useData } from 'vitepress';
+
+const route = useRoute();
+const { isDark } = useData();
+</script>
+
+<template>
+  <div style="margin-top: 24px">
+    <Giscus
+      id="comments"
+      repo="naico-wang/naico-wang.github.io"
+      repoid="R_kgDOL7bxbQ"
+      category="General"
+      categoryid="DIC_kwDOL7bxbc4Cgc7l"
+      mapping="title"
+      term="Welcome to giscus!"
+      reactionsenabled="0"
+      emitmetadata="0"
+      inputposition="top"
+      loading="lazy"
+      lang="zh-CN"
+      :theme="isDark ? 'dark' : 'light'"
+      :key="route.path"
+    />
+  </div>
+</template>
---
+++ src/.vitepress/theme/index.ts
@@ -0,0 +1,14 @@
+import DefaultTheme from 'vitepress/theme'
+import './custom.css'
+
+import { h } from 'vue';
+import GiscusComment from './components/GiscusComment.vue';
+
+export default {
+  ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+        'doc-after': () => h(GiscusComment),
+    });
+},
+}
