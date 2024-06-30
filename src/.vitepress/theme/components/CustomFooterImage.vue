c301acf7047a2bfcc7b05646bc227bcd82b87ffb
Naico Wang <>
2024-06-30 15:35:05 +0800

    feat(51la): add 51la icon

---
+++ src/.vitepress/theme/components/CustomFooterImage.vue
@@ -0,0 +1,18 @@
+<style scoped>
+.stat-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 24px 0;
+}
+</style>
+<template>
+  <div class="stat-wrapper">
+    <a
+      target="_blank"
+      title="51la网站统计"
+      href="https://v6.51.la/land/3ItqjsY11mrrfGg3">
+        <img src="https://sdk.51.la/icon/2-1.png">
+    </a>
+  </div>
+</template>
