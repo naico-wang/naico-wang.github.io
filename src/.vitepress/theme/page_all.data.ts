17b844093869532bfead0c85628b82bc052151c7
Naico Wang <wanghy1984@gmail.com>
2024-11-01 09:43:29 +0800

    fix(index): add missing react and vue content index

--- src/.vitepress/theme/page_all.data.ts
+++ src/.vitepress/theme/page_all.data.ts
@@ -1,5 +1,13 @@
 import { getAllSiteData } from '../utils/common'
 
-const dirList = ['./algorithm/**/*.md', './architect/**/*.md', './development/**/*.md', './interview/**/*.md', './pattern/**/*.md'];
+const dirList = [
+  './algorithm/**/*.md',
+  './architect/**/*.md',
+  './development/**/*.md',
+  './interview/**/*.md',
+  './pattern/**/*.md',
+  './react/**/*.md',
+  './vue/*.md'
+];
 
 export default getAllSiteData(dirList)
