4491bd242973250edaec3244bd11a8d1660c4726
Naico Wang <wanghy1984@gmail.com>
2024-11-04 18:19:15 +0800

    feat(content): add mini program silent login

--- src/.vitepress/theme/components/RecentUpdate.vue
+++ src/.vitepress/theme/components/RecentUpdate.vue
@@ -8,78 +8,110 @@ const articleData = posts.slice(0,20)
 const goToPost = (e) => go(e)
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .container {
   width: 100%;
-}
 
-.title {
-  font-size: 18px;
-  line-height: 1.2;
-  font-weight: 700;
-  padding-bottom: 20px;
-  color: var(--vp-c-brand-1);
-  border-bottom: solid 1px var(--vp-c-brand-1);
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
+  & .module-title {
+    font-size: 18px;
+    line-height: 1.2;
+    font-weight: 700;
+    padding-bottom: 20px;
+    color: var(--vp-c-brand-1);
+    border-bottom: solid 1px var(--vp-c-brand-1);
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
 
-  & .count {
-    font-size: 12px;
+    & .count {
+      font-size: 12px;
+    }
   }
-}
 
-.scroll-container {
-  max-height: 400px;
-  overflow-y: scroll;
-  scroll-behavior: smooth;
+  & .scroll-container {
+    max-height: 400px;
+    overflow-y: scroll;
+    scroll-behavior: smooth;
+
+    & .item-list {
+      padding: 0;
+      margin: 0;
+      list-style: none;
+
+      & .item {
+        margin-top: 15px;
+        cursor: pointer;
+
+        & .item-wrap {
+          padding: 15px 20px;
+          border: solid 1px #efefef;
+          border-radius: 6px;
+          color: #ffffff;
+          background-color: var(--vp-c-brand-3);
+
+          & .title {
+            font-size: 15px;
+            line-height: 18px;
+            font-weight: 700;
+          }
+
+          & .desc {
+            margin-top: 10px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            & .category {
+              padding-left: 20px;
+              font-size: 12px;
+              background: url("/icons/icon-project.svg") left center no-repeat transparent;
+              background-size: 16px;
+              font-weight: 700;
+            }
+            & .date {
+              padding-left: 20px;
+              font-size: 12px;
+              background: url("/icons/icon-program.svg") left center no-repeat;
+              background-size: 16px;
+              font-weight: 700;
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 ::-webkit-scrollbar {
   display: none;
 }
 
-.item-list {
-  padding: 0;
-  margin: 0;
-  list-style: none;
-}
-
-.item {
-  position: relative;
-  margin-top: 15px;
-  font-size: 14px;
-  line-height: 18px;
-  color: var(--vp-c-brand-1);
-  cursor: pointer;
-  transition: font-weight .1s;
-  border-bottom: dashed 1px var(--vp-c-brand-1);
-  padding-bottom: 10px;
-
-  &:hover {
-    font-size: 16px;
-    font-weight: 700;
-  }
-}
-
 @media (max-width: 740px) {
-  .title {
-    font-size: 24px;
-    line-height: 1.2;
+  .container {
+    & .module-title {
+      font-size: 24px;
+      line-height: 1.2;
+    }
   }
 }
 </style>
 
 <template>
   <div class="container">
-    <div class="title">
+    <div class="module-title">
       <div>● 最近更新</div>
       <div class="count">总计{{ count }}篇</div>
     </div>
     <div class="scroll-container">
       <ul class="item-list" v-for="(post, index) in articleData">
         <li class="item" :key="index" v-on:click="goToPost(post.url)">
-          [{{ post.category }}] · {{ post.title }}
+          <div class="item-wrap">
+            <div class="title">{{ post.title }}</div>
+            <div class="desc">
+              <span class="category">{{ post.category }}</span>
+              <span class="date">{{ post.date }}</span>
+            </div>
+          </div>
         </li>
       </ul>
     </div>
