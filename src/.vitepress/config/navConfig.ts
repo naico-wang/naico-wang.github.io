8d06ff8dacc7b8ee0be52dde2258a3d84cea1575
Naico Wang <wanghy1984@gmail.com>
2024-12-01 14:55:13 +0800

    refactor(site): refactor homepage and structure

--- src/.vitepress/config/navConfig.ts
+++ src/.vitepress/config/navConfig.ts
@@ -3,7 +3,7 @@ export const navConfig = [
   { text: '时间线', link: '/timeline', activeMatch: '/timeline'},
   { text: '博客', link: '/blog', activeMatch: '/blog' },
   { text: '面试八股文', link: '/interview', activeMatch: '/interview' },
-  { text: '专题系列文章', activeMatch: '\\b(architect|algorithm|pattern|fullstack)\\b',
+  { text: '专题', activeMatch: '\\b(architect|algorithm|pattern|fullstack)\\b',
     items: [{
       text: '全栈架构', link: '/fullstack', activeMatch: '/fullstack'
     }, {
