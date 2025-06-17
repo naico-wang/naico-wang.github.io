97880020be3ca06a7055dea159cab734ae5b64d8
Naico Wang <nwang9@lululemon.com>
2025-06-17 16:27:23 +0800

    refactor(home): refactor hero-banner

--- src/components/HeroBanner/index.tsx
+++ src/components/HeroBanner/index.tsx
@@ -6,19 +6,21 @@ const HeroBanner: React.FC = () => {
 
   return (
     <div className={styles.hero}>
-      <div className={styles.heroContent}>
-        <div className={styles.siteTitle}>Too Young, Too Simple, Sometimes Naive.</div>
-        <div className={styles.userInfo}>
-          <div className={styles.basicInfo}>
-            <div className={styles.name}>Hello, I'm <strong>Naico (Hongyu) Wang</strong>.</div>
-            <ul>
-              <li>Work as <strong>Staff Engineer - Web</strong></li>
-              <li>Talk is Cheap, Show me the <strong>CODE</strong></li>
-              <li>PPT Engineer / Confluence Engineer</li>
-              <li><strong>PMP</strong> / <strong>PMI-ACP</strong></li>
-            </ul>
+      <div className={styles.heroBlur}>
+        <div className={styles.heroContent}>
+          <div className={styles.siteTitle}>Too Young, Too Simple, Sometimes Naive.</div>
+          <div className={styles.userInfo}>
+            <div className={styles.basicInfo}>
+              <div className={styles.name}>Hello, I'm <strong>Naico (Hongyu) Wang</strong>.</div>
+              <ul>
+                <li>Work as <strong>Staff Engineer - Web</strong></li>
+                <li>Talk is Cheap, Show me the <strong>CODE</strong></li>
+                <li>PPT Engineer / Confluence Engineer</li>
+                <li><strong>PMP</strong> / <strong>PMI-ACP</strong></li>
+              </ul>
+            </div>
+            <div className={styles.avatar} />
           </div>
-          <div className={styles.avatar} />
         </div>
       </div>
     </div>
