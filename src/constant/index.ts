712fbb5b7d6ece5f85d993a694d99751e27feb34
Naico Wang <nwang9@lululemon.com>
2025-06-17 14:34:06 +0800

    refactor(home): refactor homepage

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,12 +1,12 @@
 export const HomeFeatures = [
   {
-    image: 'img/func-01.jpg',
+    image: 'img/func-01.jpeg',
     title: '系统架构设计',
     description: '常见的系统架构设计以及结构思想相关文章',
     link: '/docs/category/系统架构设计'
   },
   {
-    image: 'img/func-02.png',
+    image: 'img/func-02.jpg',
     title: '经典设计模式',
     description: '21种经典设计模式详解，原书：《深入设计模式》',
     link: '/docs/category/经典设计模式'
@@ -18,7 +18,7 @@ export const HomeFeatures = [
     link: '/docs/category/面试八股文'
   },
   {
-    image: 'img/func-04.png',
+    image: 'img/func-04.jpg',
     title: '日常读书笔记',
     description: '记录一些日常的读书笔记，以及工作心得。',
     link: '/docs/category/日常读书笔记'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,17 @@
 import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-
-import styles from './index.module.css';
-import ModuleLayout from '@site/src/components/ModuleLayout/index';
 import HeroBanner from '@site/src/components/HeroBanner/index';
-import { HomeFeatures } from '@site/src/constant/index';
+import WorkExperience from '@site/src/components/WorkExperience/index';
+import Recommend from '@site/src/components/Recommend/index';
 
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout title={siteConfig.title}>
       <HeroBanner />
-      <ModuleLayout title="推荐内容">
-        <div className={styles.content}>
-          {
-            ((HomeFeatures || []) as Array<any>).map((_, idx) => {
-              return (
-                <Link to={_.link} key={`homepage-card-${Math.random()}-${idx}`}>
-                  <div className={styles.cardItem}>
-                    <div className={styles.cardImage}>
-                      <img src={_.image} alt={_.title} />
-                    </div>
-                    <div className={styles.cardInfo}>
-                      <div className={styles.cardTitle}>{_.title.toUpperCase()}</div>
-                      <div className={styles.cardDesc}>{_.description}</div>
-                    </div>
-                  </div>
-                </Link>
-              )
-            })
-          }
-        </div>
-      </ModuleLayout>
-      <ModuleLayout title="过往工作经历">
-        <div className={styles.companyList}>
-          <img src="/img/company-grain.png" alt="Grain Tech"/>
-          <img src="/img/company-chinasoft.png" alt="ChinaSoft"/>
-          <img src="/img/company-microsoft.png" alt="Microsoft"/>
-          <img src="/img/company-farfetch.png" alt="FARFETCH"/>
-          <img src="/img/company-marriott.png" alt="Marriott International"/>
-        </div>
-      </ModuleLayout>
+      <Recommend />
+      <WorkExperience />
     </Layout>
   );
 }
